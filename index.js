33bd53f33082fa079d0a6ffa44031c786c99fb1a
Gilbert Icheboi <gilbert.icheboi@student.moringaschool>
2025-03-07 07:55:13 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(newFavorite) {
+    window.bestCustomer = newFavorite;
+}
+
+const leastFavoriteCustomer = 'some value';
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value'; 
+}
